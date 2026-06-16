d239874567324c5ba99293da08f2848cb807c019
kmax3ever <kmax3ever2@gmail.com>
2026-06-16 21:22:44 +0700

    fix

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 echo ">>> Deploy script started at $(date)"
 cd && cd xeleb-agent|| exit
 echo ">>> pull last develop at $(date)"
-git pull
+#git pull
 docker stop ai-agent || true
 docker rm ai-agent || true
 docker build -t ai-agent .
